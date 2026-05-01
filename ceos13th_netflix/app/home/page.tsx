475100148b322a11f913483a8888bb2ca85ab704
a-00-a <sy.lee.2391@gmail.com>
2026-05-01 15:37:14 +0900

    Feat: Previews 리스트 구현

--- ceos13th_netflix/app/home/page.tsx
+++ ceos13th_netflix/app/home/page.tsx
@@ -1,20 +1,25 @@
-import Header from "@/components/Header";
-import HeroCarousel from "@/feature/movies/MovieHero";
-import { movieApi } from "@/feature/movies/services/movieApi";
+import Header from '@/components/Header';
+import HeroCarousel from '@/feature/movies/MovieHero';
+import PreviewSection from '@/feature/movies/PreviewSection';
+import ContinueWatchingSection from '@/feature/movies/ContinueWatchingSection';
+import { movieApi } from '@/feature/movies/services/movieApi';
 
 export default async function Home() {
-    const top10KoreaData = await movieApi.getTop10KoreaToday();
+  const top10KoreaData = await movieApi.getTop10KoreaToday();
+  const previews = await movieApi.getNowPlaying();
+  const continueWatching = await movieApi.getPopular();
 
-    return (
-        <div className="flex w-full h-dvh justify-center bg-black">
-            <div className="w-full px-4 pt-6 z-10 absolute">
-                <Header />
-            </div>
-            
-            <div className="w-full h-100">
-                <HeroCarousel movies={top10KoreaData.results} />
-            </div>
-            
-        </div>
-    )
+  return (
+    <main className="flex w-full min-h-dvh justify-center bg-black">
+      <div className="w-full px-4 pt-6 z-10 absolute">
+        <Header />
+      </div>
+
+      <div className="w-full bg-black">
+        <HeroCarousel movies={top10KoreaData.results} />
+        <PreviewSection movies={previews.results} />
+        <ContinueWatchingSection movies={continueWatching.results} />
+      </div>
+    </main>
+  );
 }
