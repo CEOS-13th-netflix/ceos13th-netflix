4350dc6650b8d8978354831bd685a7a9ee875576
a-00-a <sy.lee.2391@gmail.com>
2026-05-01 22:43:35 +0900

    Chore: svgr, dotenv 패키지 추가

--- ceos13th_netflix/next.config.ts
+++ ceos13th_netflix/next.config.ts
@@ -1,12 +1,28 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next';
+import type { RuleSetRule } from 'webpack';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'image.tmdb.org' }
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: 'image.tmdb.org' }],
+  },
+  webpack(config) {
+    const rules = config.module?.rules as RuleSetRule[];
+
+    const fileLoaderRule = rules.find((rule) => {
+      return rule.test instanceof RegExp && rule.test.test('.svg');
+    });
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/;
+    }
+
+    rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
 
